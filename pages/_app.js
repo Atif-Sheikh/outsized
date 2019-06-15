02a7f1146f6adcddfa65684a5d076e422eecd2cb
waleed nasir <onet7241@gmail.com>
2019-06-15 15:08:20 -0700

    implemented root modal

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,34 @@ import withReduxStore from "../lib/with-redux-store";
 import withMaterial from "@utils/initMaterial";
 import { Provider } from "react-redux";
 import { SnackbarProvider } from "notistack";
+import ModalSync from "../components/Modal/Modal";
 
 class MyApp extends App {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      modelData: {}
+    };
+  }
+  openModal = modelData => {
+    this.setState({ open: true, modelData: modelData });
+  };
+  closeModal = () => {
+    this.setState({ open: false, modelData: {} });
+  };
   render() {
     const { Component, pageProps, reduxStore } = this.props;
     return (
       <Container>
         <SnackbarProvider maxSnack={3}>
           <Provider store={reduxStore}>
-            <Component {...pageProps} />
+            <ModalSync open={this.state.open} closeModal={this.closeModal} />
+            <Component
+              {...pageProps}
+              openModal={this.openModal}
+              closeModal={this.closeModal}
+            />
           </Provider>
         </SnackbarProvider>
       </Container>
